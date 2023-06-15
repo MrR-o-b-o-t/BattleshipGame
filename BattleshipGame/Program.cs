541833fa434256d390a45ac137def781460c7f04
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-06-15 15:58:18 -0500

    Add project files.

---
+++ BattleshipGame/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("*********************************************************************************");
+Console.WriteLine("The Game is simple, you will be prompted to select a point on a 10 x 10 grid.");
+Console.WriteLine("The point is made up of an X-value and a Y-value that represent one square of the grid... Please see below.");
+
+PrintGameBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("*********************************************************************************");
+
+void PrintGameBoard()
+{
+        for (int i = 11; i > 11; i--)
+        {
+            Console.WriteLine($"{i} - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"{i}  - - - - - - - - - -");
+            Console.WriteLine($"0 1 2 3 4 5 6 7 8 9 10");
+        }
+}
+
+Console.ReadKey();
